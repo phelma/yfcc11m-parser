cdc8c47aa255c48a9b45e1e5c4c3589354ecfea6
James Leigh <james_a_leigh@yahoo.co.uk>
2017-06-26 16:56:51 +0100

    rework code to avoid using scan as too slow

--- keyword-exporter.js
+++ keyword-exporter.js
@@ -9,7 +9,7 @@ let multimap = require('multimap');
 
 let outputFolder = './urls/';
 
-var writeCsv = function(name, imgs, done) {               
+var writeCsv = function(name, imgs) {               
   let writer = csvWriter();
   writer.pipe(fs.createWriteStream(outputFolder + name + '_urls.csv'));
 
@@ -53,46 +53,46 @@ module.exports = {
           let topimgs = new multimap();
           console.time(word);
 
-          db.scan(word + '*', '0', function(keys, handled) {
-            if(keys == null) {
-              // write list out to csv
-              console.timeEnd(word);
-              writeCsv(word, topimgs, nextword);
-            } else {
+          let cnt = 1;
+          async.whilst(
+            function() {return cnt >= 0;},
+            function(nextIter) {
+              let key = word + '_' + cnt;
+              db.getArray(key, function(err, value) {
+                if(err || value == null || value.length === 0) {
+                  cnt = -1;
+                  writeCsv(word, topimgs);
+                  nextIter();
+                } 
+                else {
+                  let titleCnt = Number(value[0]);
+                  let descrpCnt = Number(value[1]);
+                  let tagOrder = Number(value[2]);
+                  let url = value[3];
+                  let score = (15 - tagOrder) + titleCnt + descrpCnt;
+                  topimgs.set(score, {tag:word, url:url});
 
-              // get the key
-              async.each(keys, function(key, cb) {
-                db.getArray(key,
-                  function(err, value) {
-                    let titleCnt = Number(value[0]);
-                    let descrpCnt = Number(value[1]);
-                    let tagOrder = Number(value[2]);
-                    let url = value[3];
-                    let score = (15 - tagOrder) + titleCnt + descrpCnt;
-                    topimgs.set(score, {tag:word, url:url});
-
-                    if(topimgs.size > 4500) {
-                      // remove an image with the smallest score
-                      let key = topimgs.keys().next().value;
-                      let valueToDelete = topimgs.get(key)[0];
-                      if(!topimgs.delete(key, valueToDelete)) {
-                        console.log('failed to delte');
-                      }
-                      else {
-                        if(topimgs.get(key).length === 0) {
-                          topimgs.delete(key); 
-                        }
+                  if(topimgs.size > 4500) {
+                    // remove an image with the smallest score
+                    let key = topimgs.keys().next().value;
+                    let valueToDelete = topimgs.get(key)[0];
+                    if(!topimgs.delete(key, valueToDelete)) {
+                      console.log('failed to delte');
+                    }
+                    else {
+                      if(topimgs.get(key).length === 0) {
+                        topimgs.delete(key); 
                       }
                     }
-                    cb();
                   }
-                );
-              },
-              function(err) {
-                handled();
+                  cnt += 1
+                  nextIter();
+                }
               });
-            }
-          });
+            },
+            function() {
+              nextword();
+            });
         },
         function() {
           next();
